44a6e43e44e75c4348c0b81ef662097e4d4f0193
talkingfoxmid <futureinrevolution@gmail.com>
2020-10-11 17:15:08 +0000

    v 2.1.1

--- src/main/java/ru/eatthefrog/hatterBot/MongoDBOperator/MongoUserStatesManager.java
+++ src/main/java/ru/eatthefrog/hatterBot/MongoDBOperator/MongoUserStatesManager.java
@@ -1,7 +1,9 @@
 package ru.eatthefrog.hatterBot.MongoDBOperator;
 
+import org.springframework.stereotype.Component;
 import ru.eatthefrog.hatterBot.DialogStateManager.UserDialogStatePosition;
 
+@Component
 public class MongoUserStatesManager {
     public UserDialogStatePosition getStatePosition(int chatID) {
         return null;
