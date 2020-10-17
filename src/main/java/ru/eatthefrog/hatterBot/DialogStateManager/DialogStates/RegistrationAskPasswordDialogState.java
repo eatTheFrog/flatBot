95815f1349954e2e813e1f157461e75607d953f4
talkingfoxmid <futureinrevolution@gmail.com>
2020-10-17 22:05:54 +0000

    loginInstance factory was added

--- src/main/java/ru/eatthefrog/hatterBot/DialogStateManager/DialogStates/RegistrationAskPasswordDialogState.java
+++ src/main/java/ru/eatthefrog/hatterBot/DialogStateManager/DialogStates/RegistrationAskPasswordDialogState.java
@@ -1,30 +1,30 @@
 package ru.eatthefrog.hatterBot.DialogStateManager.DialogStates;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.eatthefrog.hatterBot.DialogStateManager.DialogStatePosition;
+import ru.eatthefrog.hatterBot.TelegramChatSTDOUT;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class RegistrationAskPasswordDialogState extends DialogState {
-
-    public DialogState getNextState(String userInput, DialogStatePosition dialogStatePosition) {
-        dialogStatePosition.loginInstance.password = userInput;
+    @PostConstruct
+    void initIdentifier() {
+        dialogStateIdentifier = "registrationAskPasswordDialogState";
+    }
+    @Autowired
+    TelegramChatSTDOUT telegramChatSTDOUT;
+    public DialogState moveOtherState(String userInput, DialogStatePosition dialogStatePosition) {
+        dialogStatePosition.loginInstance.setPassword(userInput);
         loginValidChecker.rememberLoginInDB(dialogStatePosition.loginInstance);
         loginValidChecker.checkValidLoginInMongoAndUpdateVerification(dialogStatePosition.loginInstance);
-        return getMainMenu(dialogStatePosition);
+        telegramChatSTDOUT.printInChat(
+                "You have made registration.",
+                dialogStatePosition.chatID
+        );
+        return getNextMenuState(dialogStatePosition);
     }
 
-    @Override
-    public void fillStateMap() {
 
-    }
-
-    @Override
-    public String getOutPrompt(DialogStatePosition dialogStatePosition) {
-        return String.format("User %s registered.", dialogStatePosition.loginInstance.login);
-    }
-
-    @Override
-    public String[] getResponse(String userInput, DialogStatePosition dialogStatePosition) {
-        return new String[]{getInPrompt(dialogStatePosition)};
-    }
 }
