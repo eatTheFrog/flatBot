9d994aaa94ddbb3adf879594105abed88d8c65bb
Ivan C <inhelsmith@gmail.com>
2020-12-03 20:01:40 +0500

    more tests added

--- src/test/java/DialogStatesTest/LoggedMainMenuTests.java
+++ src/test/java/DialogStatesTest/LoggedMainMenuTests.java
@@ -1,4 +1,62 @@
 package DialogStatesTest;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.eatthefrog.hatterBot.DialogStateManager.DialogStates.*;
+import ru.eatthefrog.hatterBot.SpringConfiguration;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = SpringConfiguration.class)
 public class LoggedMainMenuTests {
+    @Autowired
+    LoggedMainMenu loggedMainMenu;
+
+    @Test
+    public void testNetworking(){
+        Assert.assertTrue(
+                Utils.isNextState(loggedMainMenu,
+                "/networking",
+                NetworkingToolsMenuState.class,
+                Utils.getLoggedDSP()));
+    }
+
+    @Test
+    public void testInfo(){
+        Assert.assertTrue(
+                Utils.isNextState(loggedMainMenu,
+                "/info",
+                InfoState.class,
+                Utils.getLoggedDSP()));
+    }
+
+    @Test
+    public void testEcho(){
+        Assert.assertTrue(
+                Utils.isNextState(loggedMainMenu,
+                "/echo",
+                EchoState.class,
+                Utils.getLoggedDSP()));
+    }
+
+    @Test
+    public void testSettings(){
+        Assert.assertTrue(
+                Utils.isNextState(loggedMainMenu,
+                "/settings",
+                NotYetImplementedState.class,
+                Utils.getLoggedDSP()));
+    }
+
+    @Test
+    public void testLogout(){
+        Assert.assertTrue(
+                Utils.isNextState(loggedMainMenu,
+                "/logout",
+                LogoutState.class,
+                Utils.getLoggedDSP()));
+    }
 }
