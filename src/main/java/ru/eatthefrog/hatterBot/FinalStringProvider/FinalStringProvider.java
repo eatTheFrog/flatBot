649e10a7901aa64fd891e10a86974175f083c185
talkingfoxmid <futureinrevolution@gmail.com>
2020-12-24 17:23:42 +0500

    3.8!!

--- src/main/java/ru/eatthefrog/hatterBot/FinalStringProvider/FinalStringProvider.java
+++ src/main/java/ru/eatthefrog/hatterBot/FinalStringProvider/FinalStringProvider.java
@@ -1,9 +1,11 @@
 package ru.eatthefrog.hatterBot.FinalStringProvider;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
+import ru.eatthefrog.hatterBot.Application;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
@@ -15,11 +17,13 @@ import java.util.Hashtable;
 @Component
 public class FinalStringProvider {
     Dictionary<String, String> finalStringsDictionary = new Hashtable<>();
-
+    String[] finalStringNames = {"PingSpecificCountState","NmapDefaultState","RegistrationAskLoginDialogState","SpyVkState","NmapState","SpyVkAddSpyFriendsState","NmapPortState","NotYetImplementedState","StartingState","UnloggedMainMenu","LoggedMainMenu","LogoutState","VkTokenAskState","UnknownDialogState","InfoState","PingState","LoginAskPasswordDialogState","GetSpyListState","PingBurstState","SpyVkAddSpyOnline","LoginAskLoginDialogState","EchoState","SpyVkAddSpyWall","RegistrationAskPasswordDialogState","NetworkingToolsMenuState","NmapAllState","PingSpecificCountState","NmapDefaultState","RegistrationAskLoginDialogState","SpyVkState","NmapState","SpyVkAddSpyFriendsState","NmapPortState","NotYetImplementedState","StartingState","UnloggedMainMenu","LoggedMainMenu","LogoutState","VkTokenAskState","UnknownDialogState","InfoState","PingState","LoginAskPasswordDialogState","GetSpyListState","PingBurstState","SpyVkAddSpyOnline","LoginAskLoginDialogState","EchoState","SpyVkAddSpyWall","RegistrationAskPasswordDialogState","NetworkingToolsMenuState","NmapAllState"};
 
     @PostConstruct
     void loadFinalStrings() {
         try {
+
+            ClassLoader cl = Application.class.getClassLoader();
             File[] files = ResourceUtils.getFile("classpath:finalStrings").listFiles();
             if (files == null)
             {
@@ -27,10 +31,15 @@ public class FinalStringProvider {
                 return;
             }
 
-            for (File file: files) {
+
+            for (String name: this.finalStringNames) {
+                var inputStream = cl.getResourceAsStream("finalStrings/"+name);
+                if (inputStream == null) {
+                    return;
+                }
                 finalStringsDictionary.put(
-                        file.getName(),
-                        FileUtils.readFileToString(file, StandardCharsets.UTF_8));
+                        name,
+                        IOUtils.toString(inputStream));
             }
 
         }
