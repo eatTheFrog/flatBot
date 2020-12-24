d7019fa3b2edfc4c6c52d644ea3daf63765fd214
Ivan C <inhelsmith@gmail.com>
2020-12-24 19:34:18 +0500

    token via mongo

--- src/main/java/ru/eatthefrog/hatterBot/ExternalApiProvider/TelegramAPI/TelegramBotTokenProvider.java
+++ src/main/java/ru/eatthefrog/hatterBot/ExternalApiProvider/TelegramAPI/TelegramBotTokenProvider.java
@@ -1,9 +1,12 @@
 package ru.eatthefrog.hatterBot.ExternalApiProvider.TelegramAPI;
 
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import ru.eatthefrog.hatterBot.ExternalApiProvider.BotTokenProvider;
+import ru.eatthefrog.hatterBot.MongoUtils.MongoUtils;
 import ru.eatthefrog.hatterBot.PropertiesProvider;
 
 
@@ -15,11 +18,19 @@ public class TelegramBotTokenProvider implements BotTokenProvider {
     @Autowired
     PropertiesProvider propertiesProvider;
 
+    @Autowired
+    MongoDatabase mongoDatabase;
+
+    @Autowired
+    MongoUtils mongoUtils;
+
     public String getToken() {
-        if (token.equals("default"))
-            token = propertiesProvider.getProperty(
+        var mongoToken = mongoUtils.fetchTelegramToken(mongoDatabase);
+        if (mongoToken == null){
+            return propertiesProvider.getProperty(
                     "secret.bot.properties",
                     "telegram_token");
-        return token;
+        }
+        return mongoToken;
     }
 }
---
+++ src/main/java/ru/eatthefrog/hatterBot/MongoUtils/MongoUtils.java
@@ -0,0 +1,45 @@
+package ru.eatthefrog.hatterBot.MongoUtils;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class MongoUtils {
+    private final String TOKEN_COLLECTION = "tokenCollection";
+
+    private boolean collectionExists(MongoDatabase mongoDatabase, String collectionName){
+        for (final String name : mongoDatabase.listCollectionNames()) {
+            if (name.equalsIgnoreCase(collectionName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String getToken(MongoCollection<Document> collection, String platform){
+        Document doc = collection.find(new Document("platform", platform)).first();
+        System.out.println(doc);
+        if (doc == null)
+            return null;
+        return doc.getString("token");
+    }
+
+    private String fetchToken(MongoDatabase mongoDatabase, String platform){
+        if (! tokeCollectionExists(mongoDatabase))
+            return null;
+
+        return getToken(mongoDatabase.getCollection(TOKEN_COLLECTION), platform);
+    }
+
+    public String fetchTelegramToken(MongoDatabase mongoDatabase){
+        return fetchToken(mongoDatabase, "telegram");
+    }
+
+    private boolean tokeCollectionExists(MongoDatabase mongoDatabase){
+        return collectionExists(mongoDatabase, TOKEN_COLLECTION);
+    }
+}
